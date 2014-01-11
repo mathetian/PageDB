45bd2baa1ddeedac6f16dd0b093fc05f08ead941
mathewes <mathewes@sjtu.edu.cn>
2014-01-11 21:08:23 +0800

    Add MemoryPool

--- helpers/MemoryPool.cpp
+++ helpers/MemoryPool.cpp
@@ -1,65 +1,166 @@
 #include "MemoryPool.h"
-#include <stddef.h>
-#include <sys/sysinfo.h>
+#include <math.h>
+#include <assert.h>
+#include <stdio.h>
 
-uint64_t GetAvailPhysMemorySize()
-{   
-    struct sysinfo info;
-    sysinfo(&info);
-    return info.freeram;
-}
-
-MemoryPool::MemoryPool()
+MemoryPool::MemoryPool(size_t initPoolSize, size_t eachChunkSize) : m_eachChunkSize(eachChunkSize)
 {
-    m_pMem     = NULL;
-    m_nMemSize = 0;
-    m_nAvailPhys = GetAvailPhysMemorySize();
-
-    if(m_nAvailPhys < 16*24*24)
-        m_nMemMax = m_nAvailPhys >> 1;
-    else
-        m_nMemMax = m_nAvailPhys - 8*1024*1024;
+    firstChunk = lastChunk = curChunk = NULL;
+    m_totalMemoryPoolSize = 0;
+    m_freeMemoryPoolSize  = 0;
+    m_chunkNum = 0;
 }
 
 MemoryPool::~MemoryPool()
 {
-    if(m_pMem)
+    freeAllMemory();
+    freeAllChunks();
+}
+
+int MemoryPool::calculateNeededChunks(size_t size)
+{
+    float fnum = ((float)size)/((float)m_eachChunkSize);
+    return (int)ceil(fnum);
+}
+
+MemoryChunk * MemoryPool::skipChunk(MemoryChunk * chunk,int step)
+{
+    while(step--) 
     {
-        delete m_pMem;
-        m_pMem = NULL;
+        chunk = chunk -> nextChunk;
+        if(!chunk) break;
+    }
+    return chunk;
+}
+
+MemoryChunk * MemoryPool::findSuitableChunks(int chunkNum)
+{
+    if(m_freeMemoryPoolSize <= chunkNum * m_eachChunkSize) return NULL;
+    MemoryChunk * chunk = curChunk, * oldChunk = curChunk; int f = 1;
+    while(chunk != oldChunk || f != 0)
+    {   
+        if(chunk -> totalChunk >= chunkNum)
+        {
+            if(chunk -> usedChunk == 0)
+                return chunk;
+        }
+        int step = (chunk -> usedChunk != 0) ? (chunk -> usedChunk) : 1;
+        chunk = skipChunk(chunk, step);
+        if(chunk == NULL) chunk = firstChunk;
+        f = 0;
+    } 
+    if(chunk == oldChunk) return NULL;
+    return chunk;
+}
+
+bool MemoryPool::allocateMemory(size_t size)
+{
+    int needChunks = calculateNeededChunks(size);
+    int needSize   = needChunks * m_eachChunkSize;
+   
+    char * memoryData    = new char[needSize];
+    MemoryChunk * chunks = new MemoryChunk[needChunks];
+    
+    if(!memoryData || ! chunks)
+    {
+        if(memoryData) delete [] memoryData;
+        if(chunks) delete [] chunks;
+        memoryData = NULL; chunks = NULL;
+        fprintf(stderr,"Error : allocateMemory error, maybe run out of memory\n.");
+        return false;
+    }    
+
+    m_totalMemoryPoolSize += needSize;
+    m_freeMemoryPoolSize  += needSize;
+    
+    memset(chunks,0xff,sizeof(MemoryChunk) * needChunks);
+
+    int offset = 0, index = 0;
+    for(;index < needChunks;index++)
+    {
+        if(firstChunk == NULL)
+        {
+            firstChunk = chunks;
+            curChunk = lastChunk = chunks;
+        }
+        else
+        {  
+            lastChunk -> nextChunk = \
+                chunks + index;
+            lastChunk = chunks + index;
+        }
+        chunks[index].totalChunk = needChunks - index;
+        chunks[index].usedChunk  = 0;
+        chunks[index].data       = memoryData + offset;
+        if(index == 0) chunks[index].header = true;
+        offset += m_eachChunkSize;
+    }
+    return true;
+}
+
+char * MemoryPool::getMemory(size_t size)
+{
+    int needChunks = calculateNeededChunks(size);
+    int needSize   = needChunks * m_eachChunkSize;
+    MemoryChunk * chunk = NULL;
+    while(chunk == NULL)
+    {
+        chunk = findSuitableChunks(needChunks);
+        if(chunk == NULL)
+            allocateMemory(needSize);
+    }
+    m_freeMemoryPoolSize -= needSize;
+    chunk -> usedChunk    = needChunks;
+    return chunk -> data;
+}
+
+bool MemoryPool::freeMemory(char * data, size_t size)
+{
+    int needChunks = calculateNeededChunks(size);
+    int needSize   = needChunks * m_eachChunkSize;
+    MemoryChunk * chunk = firstChunk;
+    while(chunk && chunk -> data != data)
+        chunk =  chunk -> nextChunk;
+    
+    if(!chunk || chunk -> usedChunk != needChunks) 
+        return false;
+    
+    if(chunk -> usedChunk == needChunks)
+        chunk -> usedChunk = 0;
+    
+    return true;
+}
+
+void MemoryPool::freeAllMemory()
+{
+    MemoryChunk * chunk = firstChunk;
+    
+    while(chunk)
+    {
+        if(chunk -> header == true)
+            delete [] (chunk -> data);
+        chunk -> data = NULL;
+        chunk = chunk -> nextChunk;
     }
 }
 
-char* MemoryPool::Allocate(uint64_t nFileLen, uint64_t & nAllocatedSize)
+void MemoryPool::freeAllChunks()
 {
-    unsigned int nTargetSize;
-
-    if(nFileLen <= m_nMemSize)
+    MemoryChunk * dleChunk = NULL;
+    MemoryChunk * curChunk = firstChunk;
+    
+    while(curChunk)
     {
-        nAllocatedSize = nFileLen;
-        return m_pMem;
+        if(curChunk -> header == true)
+        {
+            if(dleChunk)
+                delete [] dleChunk;
+            dleChunk = curChunk;
+        }
+        curChunk = curChunk -> nextChunk;
     }
 
-    nTargetSize = (nFileLen < m_nMemMax) ? nFileLen : m_nMemMax;
-
-    if(m_pMem != NULL)
-    {
-        delete m_pMem;
-        m_pMem = NULL;
-        m_nMemSize = 0;
-    }
-
-    m_pMem = new char[nTargetSize];
-
-    if(m_pMem != NULL)
-    {
-        m_nMemSize     = nTargetSize;
-        nAllocatedSize = nTargetSize;
-        return m_pMem;
-    }
-    else
-    {
-        nAllocatedSize = 0;
-        return NULL;
-    }
+    if(dleChunk) 
+        delete [] dleChunk;
+    dleChunk = NULL;
 }
