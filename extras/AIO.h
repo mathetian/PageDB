094f83862c69648f55b968316482369ffbf04ae9
mathetian <mathetian@gmail.com>
2014-02-26 18:36:24 +0800

    add AIO

--- extras/AIO.h
+++ extras/AIO.h
@@ -28,12 +28,11 @@ DEFINE_int32(max_nr, 1);
 
 typedef function<void(int) > Callback;
 
-class AIORequest
-{
+class AIORequest{
 public:
     AIORequest(const char * buf, int offset, int size, Callback callback) : \
-        buf(buf), offset(offset), size(size), callback(callback) { }
-    ~AIORequest() { }
+                    buf(buf), offset(offset), size(size), callback(callback){ }
+   ~AIORequest() { }
 
 public:
     virtual void PostExecute(int status) = 0;
@@ -45,32 +44,30 @@ protected:
     Callback callback;
 };
 
-class AIOReadRequest : public AIORequest
-{
+class AIOReadRequest : public AIORequest{
 public:
-    AIOReadRequest(const char * buf, int offset, int size, Callback callback) : AIORequest(buf,offset,size,callback) { }
-    ~AIOReadRequest() { }
+    AIOReadRequest(const char * buf, int offset, int size, Callback callback) : AIORequest(buf,offset,size,callback){ }
+   ~AIOReadRequest() { }
 
 public:
     void PostExecute(int status)
     {
-        if(status != size)
+        if(status != size) 
             printf("Read Error, %d\n",status);
         if(callback)
             callback(status == size ? 1 : 0);
     }
 };
 
-class AIOWriteRequest : public AIORequest
-{
+class AIOWriteRequest : public AIORequest{
 public:
-    AIOWriteRequest(const char * buf, int offset, int size, Callback callback) : AIORequest(buf,offset,size,callback) { }
-    ~AIOWriteRequest() { }
+    AIOWriteRequest(const char * buf, int offset, int size, Callback callback) : AIORequest(buf,offset,size,callback){ }
+   ~AIOWriteRequest() { }
 
 private:
     void PostExecute(int status)
     {
-        if(status != size)
+        if(status != size) 
             printf("Write Error, %d\n",status);
         if(callback)
             callback(status == size ? 1 : 0);
@@ -79,35 +76,24 @@ private:
 
 /*class aioRequest{
 public:
-	aioRequest(struct aiocb * piocb, Callback callback) : piocb(piocb), callback(callback) { }
+    aioRequest(struct aiocb * piocb, Callback callback) : piocb(piocb), callback(callback) { }
    ~aioRequest() { }
 
 public:
-	void PostExecute()
-	{
-		if(callback)
-			callback(1);
-	}
+    void PostExecute()
+    {
+        if(callback)
+            callback(1);
+    }
 public:
-	struct aiocb * piocb;
-	Callback callback;
+    struct aiocb * piocb;
+    Callback callback;
 };*/
 
-class AIOFile
-{
+class AIOFile{
 public:
-    AIOFile()
-    {
-        fd = -1;
-        thr = NULL;
-    }
-    ~AIOFile()
-    {
-        if(thr) thr -> join();
-        if(thr) delete thr;
-        thr = NULL;
-        if(fd != -1) close(fd);
-    }
+    AIOFile() { fd = -1; thr = NULL;}
+   ~AIOFile() { if(thr) thr -> join(); if(thr) delete thr; thr = NULL; if(fd != -1) close(fd);}
 
 public:
     void AIO_Open(const char * fileName, int oflag = O_WRONLY, mode_t mode = DefaultMode)
@@ -121,12 +107,11 @@ public:
     void AIO_Close()
     {
         m_mutex.lock();
-
+        
         assert(fd != -1);
-        thr -> join();
-        thr = NULL;
+        thr -> join(); thr = NULL; 
         close(fd);
-
+        
         m_mutex.unlock();
     }
 
@@ -134,7 +119,7 @@ public:
     {
         struct iocb  iocb;
         struct iocb* iocbs = &iocb;
-
+        
         AIORequest *req = new AIOReadRequest(buf, offset, size, callback);
         io_prep_pread(&iocb, fd, buf, size, offset);
         iocb.data = req;
@@ -146,7 +131,7 @@ public:
     {
         struct iocb  iocb;
         struct iocb* iocbs = &iocb;
-
+        
         AIORequest *req = new AIOWriteRequest(buf, offset, size, callback);
         io_prep_pread(&iocb, fd, buf, size, offset);
         iocb.data = req;
@@ -155,59 +140,59 @@ public:
     }
 
 public:
-    /*	void aio_open(const char * fileName, int oflag = O_WRONLY, mode_t mode = DefaultMode)
-    	{
-    		fd = open(fileName, oflag | O_DIRECT | O_CREAT, mode);
-    		assert((fd != -1) && ("AIO_Open error\n"));
-    	}
+/*  void aio_open(const char * fileName, int oflag = O_WRONLY, mode_t mode = DefaultMode)
+    {       
+        fd = open(fileName, oflag | O_DIRECT | O_CREAT, mode);
+        assert((fd != -1) && ("AIO_Open error\n"));
+    }
 
-    	void aio_read(char * buf, int offset, int size, Callback callback = NULL)
-    	{
-    		struct aiocb my_aiocb;
-      		bzero( (char *)&my_aiocb, sizeof(struct aiocb) );
+    void aio_read(char * buf, int offset, int size, Callback callback = NULL)
+    {
+        struct aiocb my_aiocb;
+        bzero( (char *)&my_aiocb, sizeof(struct aiocb) );
 
-    	    my_aiocb.aio_buf = buf;
-    	    assert(my_aiocb.aio_buf);
+        my_aiocb.aio_buf = buf;
+        assert(my_aiocb.aio_buf);
 
-    		my_aiocb.aio_fildes = fd;
-      		my_aiocb.aio_nbytes = size;
-      		my_aiocb.aio_offset = offset;
+        my_aiocb.aio_fildes = fd;
+        my_aiocb.aio_nbytes = size;
+        my_aiocb.aio_offset = offset;
 
-      		my_aiocb.aio_sigevent.sigev_notify = SIGEV_THREAD;
-    	    my_aiocb.aio_sigevent.notify_function = aio_completion_handler;
-    	    my_aiocb.aio_sigevent.notify_attributes = NULL;
+        my_aiocb.aio_sigevent.sigev_notify = SIGEV_THREAD;
+        my_aiocb.aio_sigevent.notify_function = aio_completion_handler;
+        my_aiocb.aio_sigevent.notify_attributes = NULL;
 
-    	    aioRequest aio1(&my_aiocb, callback);
+        aioRequest aio1(&my_aiocb, callback);
 
-    	    my_aiocb.aio_sigevent.sigev_value.sival_ptr = &aio1;
+        my_aiocb.aio_sigevent.sigev_value.sival_ptr = &aio1;
 
-      		ret = aio_read( &my_aiocb );
-      		assert(ret >= 0);
-      	}
+        ret = aio_read( &my_aiocb );
+        assert(ret >= 0);
+    }
 
-    	void aio_write(const char * buf, int offset, int size, Callback callback = NULL)
-    	{
-    		struct aiocb my_aiocb;
-      		bzero( (char *)&my_aiocb, sizeof(struct aiocb) );
+    void aio_write(const char * buf, int offset, int size, Callback callback = NULL)
+    {
+        struct aiocb my_aiocb;
+        bzero( (char *)&my_aiocb, sizeof(struct aiocb) );
 
-    	    my_aiocb.aio_buf = buf;
-    	    assert(my_aiocb.aio_buf);
+        my_aiocb.aio_buf = buf;
+        assert(my_aiocb.aio_buf);
 
-    		my_aiocb.aio_fildes = fd;
-      		my_aiocb.aio_nbytes = size;
-      		my_aiocb.aio_offset = offset;
+        my_aiocb.aio_fildes = fd;
+        my_aiocb.aio_nbytes = size;
+        my_aiocb.aio_offset = offset;
 
-      		my_aiocb.aio_sigevent.sigev_notify = SIGEV_THREAD;
-    	    my_aiocb.aio_sigevent.notify_function = aio_completion_handler;
-    	    my_aiocb.aio_sigevent.notify_attributes = NULL;
+        my_aiocb.aio_sigevent.sigev_notify = SIGEV_THREAD;
+        my_aiocb.aio_sigevent.notify_function = aio_completion_handler;
+        my_aiocb.aio_sigevent.notify_attributes = NULL;
 
-    	    aioRequest aio1(&my_aiocb, callback);
+        aioRequest aio1(&my_aiocb, callback);
 
-    	    my_aiocb.aio_sigevent.sigev_value.sival_ptr = &aio1;
+        my_aiocb.aio_sigevent.sigev_value.sival_ptr = &aio1;
 
-      		int ret = aio_write( &my_aiocb );
-      		assert(ret >= 0);
-    	}*/
+        int ret = aio_write( &my_aiocb );
+        assert(ret >= 0);
+    }*/
 
 private:
     void run()
@@ -216,20 +201,20 @@ private:
 
         while(m_mutex.try_lock())
         {
-
+            
             struct timespec timeout;
             timeout.tv_sec = 10;
             timeout.tv_nsec = 0;
-
+            
             int num_events;
             num_events = io_getevents(ioctx, FLAGS_min_nr, FLAGS_max_nr, events, &timeout);
-
-            for (int i = 0; i < num_events; i++)
+            
+            for (int i = 0; i < num_events; i++) 
             {
-                struct io_event event = events[i];
-                AIORequest* req = static_cast<AIORequest*>(event.data);
-                req -> PostExecute(event.res);
-                delete req;
+              struct io_event event = events[i];
+              AIORequest* req = static_cast<AIORequest*>(event.data);
+              req -> PostExecute(event.res);
+              delete req;
             }
             m_mutex.unlock();
         }
@@ -237,23 +222,23 @@ private:
         events = NULL;
     }
 
-    /*	void aio_completion_handler(sigval_t sigval)
-    	{
-    		AIORequest * aio1 = static_cast<aioRequest*>sigval.sival_ptr;
+/*  void aio_completion_handler(sigval_t sigval)
+    {
+        AIORequest * aio1 = static_cast<aioRequest*>sigval.sival_ptr;
 
-    	  	if (aio_error( aio1 -> piocb ) == 0)
-    	  	{
-    	    	aio1 -> PostExecute();
-    	    	aio_return( aio1 -> piocb );
-    		}
-    		delete aio1;
-    		aio1 = NULL;
-    	}*/
+        if (aio_error( aio1 -> piocb ) == 0) 
+        {
+            aio1 -> PostExecute();
+            aio_return( aio1 -> piocb );
+        }
+        delete aio1; 
+        aio1 = NULL;
+    }*/
 
 private:
     int fd;
     io_context_t ioctx;
     mutable mutex m_mutex;
-    mutable thread * thr;
+    mutable thread * thr; 
 };
 #endif
